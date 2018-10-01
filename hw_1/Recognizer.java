1a8ed4043b028d7b4b51641b4eed1529bbba88fc
Edward Prokopik <ejprok@gmail.com>
2018-10-01 15:06:42 -0700

    started hw1

---
+++ hw_1/Recognizer.java
@@ -0,0 +1,90 @@
+public class Recognizer {
+
+    public static void main(String[] args) {
+        System.out.println("hey");
+
+    }
+    char token;
+    public boolean match(char t) {
+        if (t == token) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void function() {
+        match("B");
+
+    }
+
+    public void statement() {
+     
+    }
+
+    public void assmnt() {
+
+    }
+
+    public void ifstmt() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public void read() {
+
+    }
+
+    public void output() {
+
+    }
+
+    public void funcall() {
+
+    }
+
+    public void comprsn() {
+
+    }
+
+    public void exprsn() {
+
+    }
+
+    public void factor() {
+
+    }
+
+    public void oprnd() {
+
+    }
+
+    public void opratr() {
+
+    }
+
+    public void ident() {
+
+    }
+
+    public void char() {
+
+    }
+
+    public void integer() {
+
+    }
+
+    public void letter() {
+
+    }
+
+    public void digit() {
+
+    }
+
+
+}
